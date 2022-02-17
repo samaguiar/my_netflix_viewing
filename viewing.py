a220313bdf2af5f77450478c5327230474eef3e9
Sam Aguiar <samantha.aguiar3@gmail>
2022-02-17 17:54:01 -0500

    added requirements.txt, added jupyter notebook, up-dated viewing.py with function about finding access times by profile name

--- viewing.py
+++ viewing.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv('ViewingActivity.csv')
 
-def choose_user():
+def access_by_profile(data_set):
+    profile_names = data_set['Profile Name'].value_counts()
+    profile_names = profile_names.reset_index()
+    profile_names = profile_names.rename(columns={'index':'Profile', 'Profile Name':'Number of Times Accessed'})
+    fig = px.pie(profile_names, values = 'Number of Times Accessed', names ='Profile', title='Number of Times Netflix was Accessed by User')
+    return(fig.show())
+
+def choose_user(data_set):
     name = input('Which user would you look at?')
     if name == ('Tyler' or 'Bobby'):
         df_user = df[df['Profile Name'].str.contains(name)]
@@ -10,3 +18,4 @@ def choose_user():
     else:
         print('There is not a user by that name.')
 
+#access_by_profile(df)
